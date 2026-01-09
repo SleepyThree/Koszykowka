0d1e63e0307e2ca53e34549c5b632ff0e734a0b3
SleepyThree <slimok7@gmail.com>
2026-01-09 11:19:43 +0100

    Dodawanie i wyswietlanie punktow

--- app/src/main/java/com/example/lebronjames/MainActivity.java
+++ app/src/main/java/com/example/lebronjames/MainActivity.java
@@ -1,24 +1,75 @@
 package com.example.lebronjames;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView textviewPunkty;
+    private Button button, button2, button3;
+    private int punkty;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        textviewPunkty=findViewById(R.id.textviewPunkty);
+        button=findViewById(R.id.button);
+        button2=findViewById(R.id.button2);
+        button3=findViewById(R.id.button3);
+        if (savedInstanceState != null){
+            punkty = savedInstanceState.getInt("PUNKTY");
+        }
+
+        textviewPunkty.setText(String.valueOf(punkty));
+        button.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        punkty++;
+                        textviewPunkty.setText(String.valueOf(punkty));
+
+                    }
+                }
+        );
+
+        button2.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        punkty+=2;
+                        textviewPunkty.setText(String.valueOf(punkty));
+
+                    }
+                }
+        );
+
+        button3.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        punkty += 3;
+                        textviewPunkty.setText(String.valueOf(punkty));
+                    }
+                }
+        );
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("PUNKTY",punkty);
     }
 }
